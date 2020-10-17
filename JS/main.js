fcf0d89c21ea0ba770cd0b622146fa168c8c0a42
alessfm <11_AleXS@protonmail.com>
2020-10-17 16:19:01 -0300

    Add html,css,js and other files
    Html(Turtle+); css(style,reset,fonts,carousel) and js(main,jQuery,Carousel). Fonts and images too.

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+$(document).ready( _ => {
+    
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    // Fazer menu para telas < 750px; evento clique
+    $toggleCollapse.click( _ => {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-carousel; iniciar e alterar 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    });
+})
