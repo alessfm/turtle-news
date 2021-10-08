95b697c302506de99b05c4ec79c803494e7cb207
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-10-08 16:53:40 -0300

    change: format and commentary on js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,38 @@
-const responsive ={
-    0:{items: 1},
-    320:{items: 1},
-    560:{items: 2},
-    960:{items: 3},
+const responsive = {
+  0:{items: 1},
+  320:{items: 1},
+  560:{items: 2},
+  960:{items: 3},
 }
 
+// Uso de JQuery
 $(document).ready( _ => {
 
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
+  $nav = $('.nav');
+  $toggleCollapse = $('.toggle-collapse');
 
-    // Fazer menu para telas < 750px; evento clique
-    $toggleCollapse.click( _ => {
-        $nav.toggleClass('collapse');
-    });
-    
-    // owl-carousel; iniciar e alterar 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: responsive
-    });
+  // Fazer menu compresso para telas < 750px
+  $toggleCollapse.click( _ => {
+    $nav.toggleClass('collapse');
+  });
+  
+  // owl-carousel; iniciar e alterar 
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+  });
 
-    // clicar para ir ao topo
-    $('.move-up span').click( _ => {
-        $('html,body').animate({
-            scrollTop: 0
-        },1000);
-    });
+  // botão que envia para o topo
+  $('.move-up span').click( _ => {
+    $('html,body').animate({
+      scrollTop: 0
+    },1000);
+  });
 
-    // AOS inicializar
-    AOS.init();
+  AOS.init();
 });
